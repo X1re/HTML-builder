1c38b75bdb2c1fd8d515bfa290c0925bd32f1f96
X1re <infin753@gmail.com>
2023-05-06 17:55:11 +0300

    feat: solve 04-copy-directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,6 +3,11 @@ const fs = require("fs");
 const { stdout } = require("process");
 
 const folder = join(__dirname, "secret-folder");
+
+const getFileNameWithoutExt = someFile => {
+  return someFile.split(".").slice(0, -1).join(".");
+};
+
 fs.readdir(folder, { withFileTypes: true }, (err, files) => {
   if (err) {
     throw err;
@@ -14,9 +19,9 @@ fs.readdir(folder, { withFileTypes: true }, (err, files) => {
           throw err;
         }
         stdout.write(
-          `${file.name.split(".")[0]} - ${extname(file.name).slice(1)} - ${
-            stats.size / 1000
-          }kb\n`
+          `${getFileNameWithoutExt(file.name)} - ${extname(file.name).slice(
+            1
+          )} - ${stats.size / 1000}kb\n`
         );
       });
     }
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const { join } = require("path");
+const fs = require("fs/promises");
 
+const folder = join(__dirname, "files");
+const output = join(__dirname, "files-copy");
+async function copyDir() {
+  try {
+    await fs.mkdir(output, { recursive: true }, (err, __dirname) => {
+      if (err) {
+        throw err;
+      }
+    });
+    const files = await fs.readdir(folder);
+    files.forEach(file => {
+      fs.copyFile(`${folder}\\${file}`, `${output}\\${file}`);
+    });
+    console.log("Files copied!");
+  } catch {
+    console.error("Files couldn't be copied");
+  }
+}
+copyDir();
